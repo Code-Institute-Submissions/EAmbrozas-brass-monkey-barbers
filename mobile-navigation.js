ac33dcfd0a924389d0eaa072e0af6d8fcff2ea6a
EAmbrozas <ernestasambrozas@gmail.com>
2022-10-20 17:02:38 +0000

    Add background color transparent for header and background color black for header when navigationToggle function is activated

--- mobile-navigation.js
+++ mobile-navigation.js
@@ -1,8 +1,11 @@
 function navigationToggle() {
     var x = document.getElementById("mobile-menu");
+    var y = document.getElementById("home");
     if (x.style.display === "block") {
       x.style.display = "none";
+      y.style.backgroundColor = "transparent";
     } else {
       x.style.display = "block";
+      y.style.backgroundColor = "black";
     }
   }
