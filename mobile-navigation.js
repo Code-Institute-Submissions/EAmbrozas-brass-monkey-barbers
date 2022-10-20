1faa267ee9a916a96b3d59b260cf8bcbc2977322
EAmbrozas <ernestasambrozas@gmail.com>
2022-10-20 15:06:42 +0000

    Add JS script for mobile navigation menu

---
+++ mobile-navigation.js
@@ -0,0 +1,8 @@
+function navigationToggle() {
+    var x = document.getElementById("mobile-menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
